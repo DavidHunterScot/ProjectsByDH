b547ecfd5cb0ae7fa5e43054c7c1c9e6d5b08687
DavidHunterScot <david.hunter@echoverse.cc>
2023-02-06 19:40:16 +0000

    Started working on a landing page

---
+++ src/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Projects by David Hunter</title>
+
+        <link rel="stylesheet" type="text/css" href="/assets/webfonts/poppins/poppins.css">
+
+        <style type="text/css">
+            *,
+            *::before,
+            *::after
+            {
+                box-sizing: border-box;
+            }
+
+            html,
+            body
+            {
+                margin: 0;
+                padding: 0;
+                font-family: "Poppins", sans-serif;
+                font-size: 16px;
+                background-color: #222;
+                color: #eee;
+            }
+
+            a
+            {
+                color: #fff;
+                font-weight: 600;
+            }
+
+            .container
+            {
+                max-width: 1200px;
+                margin: 0 auto;
+                padding: 1rem;
+            }
+
+            header
+            {
+                padding: 6rem 0;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <div class="container">
+                <p style="letter-spacing: 3px; font-size: 1.2rem;">Projects by</p>
+                <h1 style="font-size: 3rem;">David Hunter</h1>
+                <p>More about <a href="https://davidhunter.scot" target="_blank">David</a></p>
+            </div>
+        </header>
+    </body>
+</html>
