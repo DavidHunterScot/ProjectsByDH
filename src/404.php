17ec2b33b9df4b34f246c65bb3e0154268ced972
DavidHunterScot <david.hunter@echoverse.cc>
2023-02-08 15:26:21 +0000

    Added Not Found (404) page.

---
+++ src/404.php
@@ -0,0 +1,22 @@
+<?php $friendly_urls = false; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Not Found (404) - Projects by David Hunter</title>
+
+        <link rel="stylesheet" type="text/css" href="/assets/webfonts/poppins/poppins.css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
+    </head>
+
+    <body>
+        <div style="max-width: 800px; margin: 0 auto; padding: 1rem;">
+            <h1>Not Found (404)</h1>
+            <p>That's an error, it means nothing can be found here. You can continue looking manually, or try the <a href="/">home</a> page (which is usually much easier).</p>
+            <hr>
+            <p><i>Projects by David Hunter. Copyright &copy; David Hunter.</i></p>
+        </div>
+    </body>
+</html>
